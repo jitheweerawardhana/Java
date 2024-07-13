17f43620f89fa55ad2fc92e651181f3ae676bccf
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-13 11:07:01 +0530

    Update Demo7.java

--- Demo7.java
+++ Demo7.java
@@ -24,9 +24,20 @@ class Demo7{
 	System.out.print("Enter 4th Employee Salary : ");
 	int salary4 = scan.nextInt();
 	
-	int avgSalary = (salary1+salary2+salary3+salary4)/4;
+	double avgSalary = (salary1+salary2+salary3+salary4)/4;
 	
-	System.out.print("Average Salary is  "+avgSalary);
+	System.out.println("Average Salary is  "+avgSalary);
 	
+	double newSalary1 = salary1 + (salary1*0.04);
+	System.out.println(name1 +"new Salary is  "+newSalary1);
+	
+	double newSalary2 = salary2 + (salary2*0.04);
+	System.out.println(name2 +"new Salary is  "+newSalary2);
+	
+	double newSalary3 = salary3 + (salary3*0.04);
+	System.out.println(name3 +"new Salary is  "+newSalary3);
+
+	double newSalary4 = salary4 + (salary4*0.04);
+	System.out.println(name4 +"new Salary is  "+newSalary4);
 }
 }
