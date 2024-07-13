22ec8b5c3afd5226288ef491004c6880f68ad5dd
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-13 10:30:23 +0530

    Create Demo5.java

---
+++ Demo5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Demo5{
+	public static void main(String[] args){
+	Scanner scan = new Scanner(System.in);
+	System.out.print("Enter your age : " );
+	int age = scan.nextInt();
+	
+	System.out.println("Your Age is "+age);
+	}
+	}
