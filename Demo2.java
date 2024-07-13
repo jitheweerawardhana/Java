1959e4a241d1c2d9dcbc1b78d298e509f7b07a50
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-13 10:48:44 +0530

    Create Demo2.java

---
+++ Demo2.java
@@ -0,0 +1,15 @@
+class Demo2{
+	public static void main(String[] args){
+		
+	String studentName = "Jithendra";
+	double chemistry = 80.5;
+	double physics = 90.2;
+	double mathes = 60.3;
+	
+	double average = ( chemistry + physics + mathes )/ 3;
+	
+	System.out.println(studentName+" your average is "+average);
+	
+	}
+}
+  
