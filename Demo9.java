212361c5f027c8d9a9f0f2d7cde0007768c255e8
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-21 11:02:41 +0530

    Create Demo9.java

---
+++ Demo9.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Demo9{
+	public static void main (String[]args){
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter your age: ");
+		int age = scan.nextInt();
+		
+		if(age>18){
+			System.out.println("You are Eligible");
+			}
+		
+		}
+	}
