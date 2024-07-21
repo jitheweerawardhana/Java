c0e84c7472046e1310990b3d42b4064ed4c7d0cf
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-21 11:25:16 +0530

    Create Demo10.java

---
+++ Demo10.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Demo10{
+	public static void main (String[]args){
+	
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.print("Enter Your Name : ");
+	String name = sc.next();
+	
+	System.out.print("Enter Your Main income : ");
+	double mainIncome = sc.nextDouble();
+	
+	System.out.print("Enter Your Business income : ");
+	double businessIncome = sc.nextDouble();
+	
+	System.out.print("Enter Your Other income : ");
+	double otherIncome = sc.nextDouble();
+	
+	double totalIncome = mainIncome + businessIncome + otherIncome;
+	
+	if (totalIncome<50000){
+		System.out.println("Mrs/Mr "+name+" ,You are Aswasuma Eligible");
+		}
+		}
+	}
