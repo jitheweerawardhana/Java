726a10891d3d09e752867add4b06bd47e18ffa3a
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-21 11:02:51 +0530

    Update Demo2.java

--- Demo2.java
+++ Demo2.java
@@ -1,7 +1,7 @@
 class Demo2{
 	public static void main(String[] args){
 		
-	String studentName = "Jithendra";
+	/*String studentName = "Jithendra";
 	double chemistry = 80.5;
 	double physics = 90.2;
 	double mathes = 60.3;
@@ -10,6 +10,17 @@ class Demo2{
 	
 	System.out.println(studentName+" your average is "+average);
 	
+	char vall = 'A';
+	byte val12 = (byte)Vall;
+	System.out.ln(val12);
+	
+	
+	*/
+	int val1 = 10;
+	double val2 = val1;
+	System.out.println(val2);
 	}
-}
+	
+	}
+
   
