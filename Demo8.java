32446c6289ad4cd8f4a9b5f1fb5c563349f12c4f
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-21 11:02:29 +0530

    Update Demo8.java

--- Demo8.java
+++ Demo8.java
@@ -11,6 +11,16 @@ class Demo8{
 	System.out.println("Your new amount is " +newAmount);
 	}
 }
+
+class new{
+	char vall = 'A';
+	byte val12 = (byte)Vall;
+	System.out.ln(val12);
+	
+	int val1 = 125;
+	double val2 = val1;
+	System.out.println(val2);
+	}
 	
 	/*
 	 Python has premaid softwares
@@ -29,6 +39,15 @@ class Demo8{
 	 char
 	 
 	 boolean
+	 
+	 casting 
+	 int x2 = 20;
+	 
+	 short s1 = (byte)x2;
+	 
+	 flow controler - if, else, else if, 
+	 
+	 if condition - ture/false/ a statement that genarate true or fales 
 	  */
 
 	
