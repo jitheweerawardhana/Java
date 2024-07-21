c4e1a17f0075baef21616bb3339ba94cdd8867f3
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-21 10:02:07 +0530

    Update Demo5.java

--- Demo5.java
+++ Demo5.java
@@ -2,10 +2,24 @@ import java.util.Scanner;
 
 class Demo5{
 	public static void main(String[] args){
-	Scanner scan = new Scanner(System.in);
+	/*Scanner scan = new Scanner(System.in);
 	System.out.print("Enter your age : " );
 	int age = scan.nextInt();
 	
-	System.out.println("Your Age is "+age);
+	System.out.println("Your Age is "+age);*/
+	
+	
+	//Casting & Convertion
+	
+	byte b1 = 10;
+	int val1 = b1;
+	
+	int val2 = 20;
+	byte b2 = (byte)val2;
+
+	int y = 129;
+	byte x = (byte)y;
+	System.out.println(x);
+	
 	}
 	}
