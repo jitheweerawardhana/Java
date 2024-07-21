9ba08fd983618e88568c562b12b9f23c4af2eae0
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-21 10:02:17 +0530

    Update Demo3.java

--- Demo3.java
+++ Demo3.java
@@ -1,5 +1,36 @@
 class Demo3{
 	public static void main(String[] args){
+		
+		// single lone comment
+		/* Comment for long tearm
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * d it */
+		 
+	
 	System.out.println("Hellow");
+	System.out.print("Hallow Jithendra.......");
+	System.out.print("do it again");
+	
+	/*
+	 *  int ---> data type
+	 * studentAge ---> variable name
+	 * 24 ---> data 
+	 * */
+	
+	int studentAge = 24;
+	
+	double rainData = 52.5;
+	
+	String myName = "Jithendra";
+	
+	char mathesGrade = 'A';
+	
+	boolean isSmaoke = true;
 	}
 }
