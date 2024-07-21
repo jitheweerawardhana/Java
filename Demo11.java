155f9ef60b7771cf70c4a3b9511a30e0d610a6a3
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-21 11:46:25 +0530

    Create Demo11.java

---
+++ Demo11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Demo11{
+	public static void main (String[]args){
+	
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.print("Enter Your Name : ");
+	String name = sc.next();
+	}
+}
