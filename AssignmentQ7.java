cb3719f2fb3ebe85003196260d246c70b7a6160d
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-25 20:27:14 +0530

    Create AssignmentQ7.java

---
+++ AssignmentQ7.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class AssignmentQ6{
+	public static void main(String[]args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter lagna (aries,taurus,cancer...)  - ");
+		String lagna = sc.next().toLowerCase();
+	}
+}
