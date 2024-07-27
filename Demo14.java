18f966777e20d25a445312f05053c553f7c49eea
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-27 09:22:52 +0530

    Create Demo14.java

---
+++ Demo14.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Demo14{
+	public static void main(String[]args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter your name - ");
+		String name = sc.next();
+		
+		String nethu;
+		
+		if (name.equals("nethu")){
+			System.out.print("You are lucky");
+			}
+		else{
+			System.out.print("You are unlucky");
+			}
+		}}
+		
+		
