115663b63366b4434fb57535b2558392f80b6980
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-27 08:58:41 +0530

    Update Demo13.java

--- Demo13.java
+++ Demo13.java
@@ -4,4 +4,16 @@ class Demo13{
 	public static void main(String[]args)
 	{
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter number - ");
+		int num = sc.nextInt();
+		
+		int resalt = num%2 ;
+		
+		if (resalt == 0){
+			System.out.print("Even number");
+			}
+		else{
+			System.out.print("Odd number");
+			}
 }}
