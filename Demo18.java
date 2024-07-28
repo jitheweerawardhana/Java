f05825f9c3198dcc910b71bcb2f82af6d994d435
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-28 22:06:52 +0530

    Create Demo18.java

---
+++ Demo18.java
@@ -0,0 +1,22 @@
+class Demo18{
+	public static void main(String[]args)
+	{
+		int val = 2;
+		
+		switch(val){
+			case 1:
+			System.out.println("Hello");
+			break;
+			case 2:
+			System.out.println("Hiiii");
+			break;
+			case 5:
+			System.out.println("yooo");
+			break;
+			default:
+			System.out.println("No value matched");
+			}
+			
+	
+}
+}
