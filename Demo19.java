30ba5238aa62c1986f6945e7570302c1f7d632b0
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-28 22:07:08 +0530

    Create Demo19.java

---
+++ Demo19.java
@@ -0,0 +1,18 @@
+
+class Demo19{
+	public static void main(String[]args)
+	{
+		int num1 = 2;
+		num1+=5;
+		System.out.println(num1);
+		
+		int num2 = 5;
+		num2++;
+		System.out.println(num2);
+		
+		int num3 = 3;
+		num3 = num3 + 5;
+		System.out.println(num3);
+		
+		}
+		}
