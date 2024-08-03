dfb73374ff98413461f09391f0fc9ff0996ce867
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-03 10:24:33 +0530

    Create Demo22.java

---
+++ Demo22.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Demo22{
+	public static void main(String[]args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int numberOfFamily = 0;
+		
+		for(int i=0;i<18  ;i++){
+			System.out.println("Enter your salary - ");
+			
+			int salary = sc.nextInt();
+			
+			if(salary<35000){
+				numberOfFamily+=1;
+				}
+			}
+			
+			System.out.println(numberOfFamily);
+		
+	}
+	
+	}
