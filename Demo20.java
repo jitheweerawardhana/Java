3d232627c93e223dee344ede471f121a9693af9d
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-03 10:24:46 +0530

    Create Demo20.java

---
+++ Demo20.java
@@ -0,0 +1,28 @@
+class Demo20{
+	public static void main(String[]args)
+	{
+		int vall = 8;
+		vall+=2;
+		vall+=2;
+		System.out.println(vall);
+		
+		int val2 = 2;
+		System.out.println(val2++);
+		System.out.println(val2);
+		
+		int val3 = 6;
+		System.out.println(++val3);
+		
+		///////////////////////////////
+		
+		int val4 =78;
+		
+		if(true){
+			System.out.println(val4);
+			
+			int val5= 67;
+			}
+			
+			System.out.prinyln(vall5);
+		}
+}
