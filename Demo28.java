1185c4628b61bcedbf65d7994a6082fe53a5b261
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-10 11:30:49 +0530

    Update Demo28.java

--- Demo28.java
+++ Demo28.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+class Demo28 {
+    public static void main(String[] args) {
+		  Scanner sc = new Scanner(System.in);
+		  
+		 /* int age5 =27;
+		  * int age1 =7;
+		  * int age2 =85;
+		  * int age3=66;
+		  * int age4 =35;
+		  * 
+		  * int [] ages = new int[5];
+		  * 
+		  * ages[0] = 7;
+		  * ages[3] = 27;
+		  * 
+		  * */
+		  int [] ages = new int[5];
+		  
+		  for (int i=0;i<5;i++){
+			  System.out.print("Enter a value - ");
+			  
+			  int val = sc.nextInt();
+			  ages[i] = val;
+			  
+			  System.out.print(ages);
+			  } 
+}} 
