eae2dd866d9b244bcbb1e171e25a89408658d405
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-10 09:06:40 +0530

    Update Demo25.java

--- Demo25.java
+++ Demo25.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+class Demo25 {
+    public static void main(String[] args) {
+		  
+		  Scanner scan = new Scanner(System.in);
+		  
+		  int val = 8;
+		  boolean val2 = true;
+		  
+		  //true false statement
+		  
+		  while(val2){
+			  //task
+			  if(){
+				  
+				  
+				  val2 = false;
+				  }
+			  }
+		  
+		
+		}
+    }
