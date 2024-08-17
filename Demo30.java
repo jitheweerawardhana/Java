300483c3032118051bcde1c549375d526b365e98
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-17 08:51:11 +0530

    Update Demo30.java

--- Demo30.java
+++ Demo30.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+class Demo30 {
+    public static void main(String[] args) {
+		  Scanner sc = new Scanner(System.in);
+		  
+		  
+		  double [] sallery = new double[7];
+		  
+		  for(int i=0;i<7;i++){
+			  
+				  System.out.print("Enter your sallery - ");
+				  sallery[i] = sc.nextDouble();  
+			  }
+			  
+			for(int i=0;i<7;i++){
+				
+				sallery[i] = sallery[i]+(sallery[i]*0.12);
+				
+				}
+			for(int i=0;i<7;i++){
+				
+				System.out.println("Your new sallery - "+sallery[i]);
+				
+				}
+		
+		  }
+	}
