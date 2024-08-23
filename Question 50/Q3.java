a28f2369914e2743358ea22473da432b09289cf6
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-23 13:42:28 +0530

    Update Q3.java

--- Question 50/Q3.java
+++ Question 50/Q3.java
@@ -4,8 +4,43 @@ class Q2 {
     public static void main(String[] args) {
 		
 		  Scanner sc = new Scanner(System.in);
+		  int [][] attendence  = new int[30][22];
+		  int totalAtt [] = new int [30];
+		  
+		  int  totalAtt = 0;
 		  
 		  
-		  
+		  for( int i=0;i<30;i++ ){
+			  
+			  for( int x=0;x<22;x++ ){
+				  
+				  System.out.print("Enter your "+(x+1)+" day attendence(1 for present, 0 for absent) - ");
+				  attendence [i][x] = sc.nextInt();
+				  
+				  }
+			  }
+			  
+		  for( int i=0;i<30;i++ ){
+			  
+			  for( int x=0;x<22;x++ ){
+				  
+				 totalAtt [i] += attendence [i][x];
+				  
+				  }
+				  
+			  }
+			  
+		 int avgAtt [] = new int [30];
+		 int avgAtt  =  0;
+		 
+		 for(int i=0;i<30;i++){
+				 
+				 avg[i] = sum[i]/5;
+				 
+				 }
+			  
+		
+			  
+		
      }
   }
