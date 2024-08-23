9864d715963a762e45d9f80dc0e8220b84347e70
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-23 14:33:25 +0530

    Update Q4.java

--- Question 50/Q4.java
+++ Question 50/Q4.java
@@ -1,1 +1,48 @@
+import java.util.Scanner;
 
+class Q4 {
+    public static void main(String[] args) {
+		
+		  Scanner sc = new Scanner(System.in);
+		  
+		  int mark [] = new int [5];
+		  
+		  int countA = 0;
+		  int countB = 0; 
+		  int countC = 0; 
+		  int countD = 0;
+		  int countF = 0;
+		  
+		  for(int i=0; i<5; i++){
+			  
+			  System.out.print("Enter your marks - ");
+			  mark [i] = sc.nextInt();
+			  
+			  if( mark [i] >= 90){
+				  
+				  countA++;
+				  }
+				  
+			  else if ( mark [i] >= 80){
+				  countB++;
+				  }
+				  
+			else if ( mark [i] >= 70){
+				  countC++;
+				  }
+			else if ( mark [i] >= 60){
+				  countD++;
+				  }
+			else {
+				countF++;
+				
+				}
+			  
+			  }
+			  
+		System.out.print("Grade A sutdent count" + countA);
+		System.out.print("Grade A sutdent count" + countB);
+		System.out.print("Grade A sutdent count" + countC);
+		System.out.print("Grade A sutdent count" + countD);
+		System.out.print("Grade A sutdent count" + countF);
+}}
