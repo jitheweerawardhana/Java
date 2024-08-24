3bb0d444d6cc13d36418d3b207568af4bd29cb0e
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-24 10:51:38 +0530

    Create Demo32.java

---
+++ Demo32.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Demo32 {
+    public static void main(String[] args) {
+		
+		int []ar = {20,50,44,21,32}
+		
+		for(int data : ar){
+			System.out.print(data);
+		}
+		
+		/////
+		
+		int[] data = new int[12];
+		System.out.print(data);
+		
+		//Reference variable = array variable
+}}
