92f254ff80c6ced0c32c05767418e39e21410077
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-24 13:09:24 +0530

    Create Demo37.java

---
+++ Demo37.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.*; 
+
+class Demo37 {
+    public static void main(String[] args) {
+		
+		int [] data = {45,12,34,56,78,42,67};
+		
+		int [] result = reverseArr(data);
+		  
+		  
+          for (int i=0;i<7;i++) {
+            System.out.print(" "+result[i]);
+		  }
+	  }
+		public static int[] reverseArr(int[] data){
+			
+			int [] reverseArr = new int[data.length];
+			
+			for(int i=0;i<7;i++){
+				reverseArr[i] = data[6-i];
+        }
+        return reverseArr;
+			}
+	}
