68cd57f7dd1fb1abcc0430bebfe576dfd5f48d9f
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-24 10:51:45 +0530

    Create Demo33.java

---
+++ Demo33.java
@@ -0,0 +1,18 @@
+//Method
+
+/*  void ---> return type
+ * saveData ---> method name 
+ * () ---> parameter bracket
+ * {} ---> method scope
+ * 
+ *  */
+  public static void saveData(){
+    int data1 = 12;
+    int data2 = 13;
+    System.out.print(data1+data2);
+ }
+ 
+ //method calling
+ 
+ saveData();
+ saveData();
