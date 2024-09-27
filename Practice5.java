673944a46225f4dee2679597f993ea6eda3accc8
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-09-27 23:14:14 +0530

    Create Practice5.java

---
+++ Practice5.java
@@ -0,0 +1,38 @@
+class Practice5{
+	public static void main(String[] args){
+		
+        int totalSteps = 0;
+        
+        for (int steps = 100; steps <= 1000; steps += 100) {
+            totalSteps += steps;
+            System.out.println("Steps walked: " + totalSteps);
+        }
+    }
+}
+public class FitnessApp2 {
+
+    public static void main(String[] args) {
+        int totalSteps = 0;
+
+        while (totalSteps < 2000) {
+            totalSteps += 500;
+            System.out.println("Total steps: " + totalSteps);
+        }
+    }
+}
+
+public class FitnessApp1 {
+
+    public static void main(String[] args) {
+        int totalSteps = 0;
+        
+        for (int steps = 100; totalSteps < 1000; steps += 100) {
+            totalSteps += steps;
+            System.out.println("Steps walked: " + totalSteps);
+
+            if (totalSteps % 300 == 0) {
+                System.out.println("Notification sent!");
+            }
+        }
+    }
+}
