6f44d4b486fe359c7fe6c009ad77e9cff6af8e54
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-09-30 22:10:24 +0530

    Create Practice5.1.java

---
+++ Practice5.1.java
@@ -0,0 +1,13 @@
+public class Practice5.1 {
+
+    public static void main(String[] args) {
+        int totalSteps = 0;
+
+        // While loop that keeps running as long as total steps are less than 2000
+        while (totalSteps < 2000) {
+            totalSteps += 500;
+            System.out.println("Total steps: " + totalSteps);
+        }
+    }
+}
+
