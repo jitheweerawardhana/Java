332bf2b3e56cd3e89a91e2072e96b4ec48123d83
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2025-01-12 08:38:18 +0530

    uodated

--- studetn_management_system/src/main/java/org/example/studetn_management_system/service/impl/StudentServiceImpl.java
+++ studetn_management_system/src/main/java/org/example/studetn_management_system/service/impl/StudentServiceImpl.java
@@ -1,25 +1,27 @@
 package org.example.studetn_management_system.service.impl;
 
-import com.mysql.cj.xdevapi.SessionFactory;
+import org.hibernate.SessionFactory;
 import org.example.studetn_management_system.dto.StudentDto;
 import org.example.studetn_management_system.entity.Student;
 import org.example.studetn_management_system.service.StudentService;
 import org.example.studetn_management_system.utill.GetSessionFactory;
 import org.hibernate.Session;
 
+
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentServiceImpl implements StudentService {
 
-    private final GetSessionFactory getSessionFactory;
+   private GetSessionFactory getSessionFactory = new GetSessionFactory();
 
-    public StudentServiceImpl() {
-        this.getSessionFactory = new GetSessionFactory();
-    }
+
+
 
     @Override
     public StudentDto addStudent(StudentDto studentDto) {
-        SessionFactory sessionFactory = getSessionFactory.getSessionFactory();
+        SessionFactory sessionFactory = (SessionFactory) getSessionFactory.getSessionFactory();
         try(Session session = sessionFactory.openSession()){
             session.beginTransaction();
 
@@ -36,8 +38,8 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public StudentDto updateStudent(StudentDto studentDto) {
-        SessionFactory sessionFactory = getSessionFactory.getSessionFactory();
-        try(Session session = getSessionFactory.getSessionFactory()){
+
+        try(Session session = (Session) getSessionFactory.getSessionFactory()){
             session.beginTransaction();
 
             Student currentStudent = session.createQuery("FROM Student where id =:id",Student.class)
@@ -65,7 +67,7 @@ public class StudentServiceImpl implements StudentService {
     @Override
     public boolean deleteStudent(Integer id) {
         boolean status;
-        SessionFactory sessionFactory = getSessionFactory.getSessionFactory();
+        SessionFactory sessionFactory = (SessionFactory) getSessionFactory.getSessionFactory();
         try(Session session = sessionFactory.openSession()){
             session.beginTransaction();
 
@@ -87,11 +89,36 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public List<StudentDto> getAllStudents() {
-        return List.of();
+        List<StudentDto> studentDtoList = new ArrayList<>();
+        SessionFactory sessionFactory = (SessionFactory) getSessionFactory.getSessionFactory();
+        try(Session session = sessionFactory.openSession()){
+            session.beginTransaction();
+
+            List<Student> studentList = session.createQuery("FROM Student",Student.class).list();
+            for(Student student : studentList){
+                studentDtoList.add(new StudentDto(student.getId(),student.getName(),student.getAge()));
+            }
+            session.getTransaction().commit();
+        }
+        return studentDtoList;
     }
 
     @Override
     public StudentDto getStudentById(int id) {
-        return null;
+        StudentDto studentDto;
+        SessionFactory sessionFactory = (SessionFactory) getSessionFactory.getSessionFactory();
+        try(Session session = sessionFactory.openSession()){
+            session.beginTransaction();
+
+            Student student = session.get(Student.class,id);
+            if(student == null){
+                throw new IllegalAccessException("Student with ID "+ id + "not found.");
+            }
+            studentDto = new StudentDto(student.getId(),student.getName(),student.getAge());
+            session.getTransaction().commit();
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        return studentDto;
     }
 }
